6d9f2c20409ebb2672b1398d729ffcffd9aee07f
Lennard End <endle@bbcag.ch>
2025-10-29 08:03:34 +0100

    fixed issue with arguments

--- docker/get_executable.py
+++ docker/get_executable.py
@@ -3,16 +3,10 @@ import os
 import subprocess
 import re
 
-debug = False
-test = False
-exe_name = ""
-
-def run_make(script_arg: str = ""):
+def run_make(script_arg: str = "", debug: bool = False):
     if debug:
         script_arg = f"gdb --tui {exe_name}"
-    elif test:
-        script_arg = "make test"
-    elif not (script_arg.startswith("./") or script_arg.startswith("/")):
+    elif not (len(script_arg) == 0 or script_arg.startswith("./") or script_arg.startswith("/")):
         script_arg = f"./{script_arg}"
     
     print(f"starting make.sh with argument '{script_arg}'")
@@ -22,24 +16,28 @@ def run_make(script_arg: str = ""):
         print("cancelled by user")
     except Exception as ex:
         raise ex
+    # end script after make
+    exit(0)
+
+
+debug = False
+exe_name = ""
 
 arguments = sys.argv
 for i in range(1, len(arguments)):
     argument = arguments[i]
     if argument == "--shell":
-        debug = False
-        exe_name = "/bin/sh"
-        break
+        run_make("/bin/sh")
+    if argument == "--make-test":
+        run_make("make test")
+    
     if argument == "--debug":
         debug = True
-    if argument == "--make-test":
-        test = True
     else:
         exe_name = argument
 
-if exe_name != "" or test:
-    run_make()
-    exit(0)
+if exe_name != "":
+    run_make(exe_name, debug)
 
 print("trying to get executable name from makefile")
 
@@ -70,4 +68,4 @@ with open(makefile) as file:
     
     exe_name = groups[0]
     print(f"found {exe_name}")
-    run_make(exe_name)
+    run_make(exe_name, debug)
